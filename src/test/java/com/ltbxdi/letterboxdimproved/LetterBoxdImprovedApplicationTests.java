746c186ff4b78ac9462ace5449ba32b26e6008c5
30xintegral <elvinmahmudzade04@gmail.com>
2024-11-12 00:50:01 +0400

    first commit

---
+++ src/test/java/com/ltbxdi/letterboxdimproved/LetterBoxdImprovedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltbxdi.letterboxdimproved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterBoxdImprovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
